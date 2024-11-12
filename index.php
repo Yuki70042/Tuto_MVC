476ce685d76abe290f6734681a256e5430041ee9
Yuki70042 <82507110+Yuki70042@users.noreply.github.com>
2024-11-12 15:17:50 +0100

    V0

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+include_once "vendor/autoload.php";
+
+use App\Utilitaire\Vue;
+use App\Vue\Vue_BienvenueAllerPagePrecedente;
+use App\Vue\Vue_BienvenueAllerPageSuivante;
+
+if (isset($_REQUEST["case"]))
+    $case = $_REQUEST["case"];
+else
+    $case = "defaut";
+
+if (isset($_REQUEST["action"]))
+    $action = $_REQUEST["action"];
+else
+    $action = "defaut";
+
+$Vue = new Vue();
+switch ($case) {
+    case "A":
+    case "defaut":
+        include ".\src\Controleur\caseA.php";
+        break;
+    case "B":
+        include ".\src\Controleur\caseB.php";
+        break;
+    case "maTable":
+        include ".\src\Controleur\caseMaTable.php";
+        break;
+
+}
+$Vue->afficher();
